a9d84feafadc188f7b563fc2428d7676d949a939
savchuckvadim <savchuckvadim@gmail.com>
2022-05-12 10:26:23 +0300

    change base url

--- src/modules/services/api.js
+++ src/modules/services/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
+import { BASE_URL } from "./API-KEY";
 //baseURL: 'https://sheet.best/api/sheets/4d75a8b6-0dbc-44af-8465-9cf163faacf6/'
 
 const instance = axios.create({
     
-    baseURL: 'https://script.google.com/macros/s/AKfycby4mo1ga2FHPuJ1aulWASyW0NGuHlVUpWf85lDNiLXc9ZQwO0unDciUMXipmeNzALesog',
+    baseURL: BASE_URL,
   
 })
 //url https://script.google.com/macros/s/AKfycby4mo1ga2FHPuJ1aulWASyW0NGuHlVUpWf85lDNiLXc9ZQwO0unDciUMXipmeNzALesog/exec
