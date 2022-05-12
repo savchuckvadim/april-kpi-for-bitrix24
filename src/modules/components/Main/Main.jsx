3b6b6cf62a0785c0389f3bd8e0c799808568dd70
savchuckvadim <savchuckvadim@gmail.com>
2022-05-12 15:09:19 +0300

    adaptive layout

--- src/modules/components/Main/Main.jsx
+++ src/modules/components/Main/Main.jsx
@@ -1,23 +1,26 @@
 import BaseTableContainer from "../Tables/Base-Table/Base-Table-Container"
 import StatisticsContainer from "../Statistics/Statistics-Container"
+import './Main.css'
+
 
 
 const Main = (props) => {
-    if (props.statistics) {
-        return (
-            <>
 
+    return (
+        <>
+            <div className="main__desktop">
+                {props.statistics
+                    ? <StatisticsContainer />
+                    : <BaseTableContainer />
+
+                }
+            </div>
+            <div className="main__mobile">
                 <StatisticsContainer />
-            </>
-        )
-    } else {
-        return (
-            <>
-                <BaseTableContainer />
-              
-            </>
-        )
-    }
+            </div>
+        </>
+
+    )
 
 }
 
